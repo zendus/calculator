ebdc2ff256b9bc3dcec9d8bebc8ddfc07ea698ff
zendus <uzendujmike@gmail.com>
2019-04-15 15:06:20 +0100

    practical

---
+++ app.py
@@ -0,0 +1,19 @@
+#This code is used to create a basic calculator
+#I was playing eleko while writing this simple code
+
+num_1 = float(input('Ogbeni enter the 1st number: '))
+ope = input('Biko tinye operator: ')
+num_2 = float(input('Boss man enter the 2nd number: '))
+if ope == '+':
+    print(num_1 + num_2)
+elif ope == '-':
+    print(num_1-num_2)
+elif ope == '*':
+    print(num_1*num_2)
+elif ope == '/':
+    print(num_1/num_2)
+#Maka ndi owo ga acho itinye modulus...ah gat you efi hausa
+elif ope == '%':
+    print(num_1%num_2)
+else:
+    print("Onye mpi anya, asim gi tinye operator")
