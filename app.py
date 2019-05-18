0669af01876a2f86e91e9992aa1d084d12fa0768
zendus <uzendujmike@gmail.com>
2019-05-18 12:41:57 +0100

    update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 
 num_1 = float(input('Ogbeni enter the 1st number: '))
 ope = input('Biko tinye operator: ')
-num_2 = float(input('Boss man enter the 2nd number: '))
+num_2 = float(input('Oga boss enter the 2nd number: '))
 if ope == '+':
     print(num_1 + num_2)
 elif ope == '-':
@@ -12,7 +12,7 @@ elif ope == '*':
     print(num_1*num_2)
 elif ope == '/':
     print(num_1/num_2)
-#Maka ndi owo ga acho itinye modulus...ah gat you efi hausa
+#Maka ndi owo ga acho itinye modulus.....ah gat you efi hausa
 elif ope == '%':
     print(num_1%num_2)
 else:
